83bdfb7893288b0bf3c2ac37627bb6bf907c8f06
medhanasoorya2007 <medhanasoorya2007@gmail.com>
2026-07-14 18:49:39 +0530

    Configure CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const cors = require("cors");
-
 app.use(
   cors({
     origin: "https://expense-tracker-frontend-rho-seven.vercel.app",
